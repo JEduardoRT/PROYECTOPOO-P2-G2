6dac632b6b4550399e51ec6c30500eff53c04b97
Jandry Rodriguez <jandry_eduardo@hotmail.com>
2021-01-07 22:00:14 -0500

    Ventanas

---
+++ src/main/java/com/jandryespol/loteria/InicioController.java
@@ -0,0 +1,9 @@
+package com.jandryespol.loteria;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class InicioController {
+
+
+}
---
+++ src/main/java/com/jandryespol/loteria/NombreController.java
@@ -0,0 +1,8 @@
+package com.jandryespol.loteria;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class NombreController {
+
+}
