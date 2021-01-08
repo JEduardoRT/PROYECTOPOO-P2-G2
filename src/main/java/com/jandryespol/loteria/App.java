2c4eaea2434dc12d2717b7cf93b1149c59133bc8
Jandry Rodriguez <jandry_eduardo@hotmail.com>
2021-01-08 16:26:22 -0500

    Más ventanas enlazadas

--- src/main/java/com/jandryespol/loteria/App.java
+++ src/main/java/com/jandryespol/loteria/App.java
@@ -34,5 +34,4 @@ public class App extends Application {
     public static void main(String[] args) {
         launch();
     }
-
 }
--- src/main/java/com/jandryespol/loteria/NombreController.java
+++ src/main/java/com/jandryespol/loteria/NombreController.java
@@ -7,4 +7,9 @@ public class NombreController {
     public void regresarMenu() throws IOException{
         App.setRoot("inicio");
     }
+    public void ok() throws IOException, InterruptedException{
+        App.setRoot("alineacion");
+        Thread.sleep(2000);
+        App.setRoot("juego");
+    }
 }
