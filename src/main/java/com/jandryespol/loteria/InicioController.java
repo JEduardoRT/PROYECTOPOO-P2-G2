58d60703e55f6928aa214b15d4dd394ae6be9e76
PedroPulecio <ppulecio@espol.edu.ec>
2021-01-11 11:59:33 -0500

    Se agrego un boton para salir del juego en la ventana de inicio

--- src/main/java/com/jandryespol/loteria/InicioController.java
+++ src/main/java/com/jandryespol/loteria/InicioController.java
@@ -1,18 +1,33 @@
 package com.jandryespol.loteria;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class InicioController {
+
+    @FXML
+    private Button botondesalida;
     
     @FXML
     public void iniciarJuego() throws IOException{
         App.setRoot("nombre");
     }
+    @FXML
     public void reporte() throws IOException{
         App.setRoot("reporte");
     }
+    @FXML
     public void configuracion() throws IOException{
         App.setRoot("configuracion");
+    }
+     @FXML
+    public void salirPrograma(ActionEvent e){
+    
+            
+     System.exit(0);
     }
 }
